a085883d9b23930c8898470ff29aee439023d183
juliennuellas <julien.nuellas@gmail.com>
2018-05-23 16:40:12 +0200

    First t test on ajax submission form

--- botapp/static/js/custom.js
+++ botapp/static/js/custom.js
@@ -2,10 +2,10 @@
 // Nav Management Interactions
 // -------------------------------
 
-infoButtonElt = document.getElementById("info-button");
-chatButtonElt = document.getElementById("chat-button");
-infoContainerElt = document.getElementById("info-container");
-chatContainerElt = document.getElementById("chat-container");
+var infoButtonElt = document.getElementById("info-button");
+var chatButtonElt = document.getElementById("chat-button");
+var infoContainerElt = document.getElementById("info-container");
+var chatContainerElt = document.getElementById("chat-container");
 
 // Click on Chat Button
 chatButtonElt.addEventListener("click", function() {
@@ -25,4 +25,48 @@ infoButtonElt.addEventListener("click", function() {
   chatButtonElt.style.backgroundColor = "#CFD8DC";
   infoContainerElt.style.display = "flex";
   chatContainerElt.style.display = "none";
+});
+
+// ----------------------------------
+// Ajax Interactions on Submit Button
+// ----------------------------------
+
+// Ajax Post function
+function ajaxPost(url, data, callback, isJson) {
+    var req = new XMLHttpRequest();
+    req.open("POST", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    if (isJson) {
+        // Définit le contenu de la requête comme étant du JSON
+        req.setRequestHeader("Content-Type", "application/json");
+        // Transforme la donnée du format JSON vers le format texte avant l'envoi
+        data = JSON.stringify(data);
+    }
+    req.send(data);
+}
+
+// Ajax Treatment
+var formElt = document.querySelector("form");
+
+formElt.addEventListener("submit", function(e) {
+  var data = formElt.elements.message.value
+
+  ajaxPost("http://127.0.0.1:5000/treatment", data,
+    function(response) {
+      console.log(response)
+    },
+    true
+  );
+
+  e.preventDefault();
 });
--- botapp/views.py
+++ botapp/views.py
@@ -5,7 +5,7 @@ This module manages the views
 There is only one page for this app
 """
 
-from flask import render_template, url_for
+from flask import render_template, url_for, jsonify
 from .app import app
 from .forms import MessageForm
 
@@ -15,3 +15,7 @@ def index():
     """Initiate the index page"""
     form = MessageForm()
     return render_template('index.html', form=form)
+
+@app.route('/treatment', methods=['POST'])
+def message_back():
+    return jsonify({'sentence': 'test ajax routine'})

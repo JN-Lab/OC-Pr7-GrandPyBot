08bb4a27c6c74f212b8f42fe2fe69faac334f176
juliennuellas <julien.nuellas@gmail.com>
2018-05-25 11:51:31 +0200

    Work in progress on Parser feature

--- botapp/utils/message_parse.py
+++ botapp/utils/message_parse.py
@@ -1,33 +1,58 @@
 #! /usr/bin/env python3
 # coding: utf-8
+import unicodedata
 from string import punctuation
 from .stop_words import common_words, welcome_words
 
 class MessageParser:
 
     def remove_punctuation(self, message):
-        message = ''.join([letter for letter in message if letter not in punctuation])
-        return message
+        final_message = []
+        for letter in message:
+            if letter not in punctuation:
+                final_message.append(letter)
+            else:
+                if letter == "'" or letter == "-":
+                    letter = " "
+                    final_message.append(letter)
+
+        final_message = ''.join(final_message)
+
+        return final_message
 
     def remove_useless_words(self, message):
         # probleme sur lalgo quand on doit supprimer deux mots qui se suivent. Logique...
         split_message = message.lower().split()
+        final_message = []
         print(split_message)
         for word in split_message:
-            if word in common_words:
-                split_message.remove(word)
-
-        final_message = ' '.join(split_message)
+            if word not in common_words:
+                final_message.append(word)
+        print(final_message)
+        final_message = ' '.join(final_message)
         return final_message
+
+    def remove_accents(self, message):
+        try:
+            message = unicode(message, 'utf-8')
+        except NameError:
+            pass
+        message = unicodedata.normalize('NFD', message)
+        message = message.encode('ascii', 'ignore')
+        message = message.decode('utf-8')
+        return str(message)
 
     def remove_civilities(self, message):
         # Meme probleme qu'au dessus
+        message.lower()
+        message = self.remove_accents(message)
         message = self.remove_punctuation(message)
-        split_message = message.lower().split()
-        print(split_message)
+        message = self.remove_useless_words(message)
+        split_message = message.split()
+        final_message = []
         for word in split_message:
-            if word in welcome_words:
-                split_message.remove(word)
+            if word not in welcome_words:
+                final_message.append(word)
 
-        final_message = ' '.join(split_message)
+        final_message =' '.join(final_message)
         return final_message
--- tests/utils/test_message_parse.py
+++ tests/utils/test_message_parse.py
@@ -4,28 +4,38 @@ from botapp.utils.message_parse import MessageParser
 
 class TestMessageParser:
     MESSAGE1 = "Bonjour bot!! Ou se trouve la Tour Eiffel?"
-    MESSAGE2 = "Salut GrandPy Est-ce que tu connais l'adresse de l'Opera de Paris?"
-    MESSAGE3 = "Je souhaiterais aller au 59 boulevard de Strabourg, 75010 Paris s'il te plait."
+    MESSAGE2 = "Salut GrandPy Est-ce que tu connais l'adresse de l'Opéra de Paris?"
+    MESSAGE3 = "Je souhaiterais aller au 59 boulevard de Strasbourg, 75010 Paris s'il te plaît."
     MESSAGE4 = "Je veux allez visiter la cathédrale de notre-dame s'il vous plait."
-    MESSAGE5 = "Bonjour, quelle est l'adresse de l'hopital Georges Pompidou?"
+    MESSAGE5 = "Bonjour, quelle est l'adresse de l'hôpital Georges Pompidou?"
 
     PARSER = MessageParser()
 
     def test_remove_punctuation(self):
         assert self.PARSER.remove_punctuation(self.MESSAGE1) == "Bonjour bot Ou se trouve la Tour Eiffel"
-        assert self.PARSER.remove_punctuation(self.MESSAGE2) == "Salut GrandPy Estce que tu connais ladresse de lOpera de Paris"
-        assert self.PARSER.remove_punctuation(self.MESSAGE3) == "Je souhaiterais aller au 59 boulevard de Strabourg 75010 Paris sil te plait"
-        assert self.PARSER.remove_punctuation(self.MESSAGE4) == "Je veux allez visiter la cathédrale de notredame sil vous plait"
+        assert self.PARSER.remove_punctuation(self.MESSAGE2) == "Salut GrandPy Est ce que tu connais l adresse de l Opéra de Paris"
+        assert self.PARSER.remove_punctuation(self.MESSAGE3) == "Je souhaiterais aller au 59 boulevard de Strasbourg 75010 Paris s il te plaît"
+        assert self.PARSER.remove_punctuation(self.MESSAGE4) == "Je veux allez visiter la cathédrale de notre dame s il vous plait"
+        assert self.PARSER.remove_punctuation(self.MESSAGE5) == "Bonjour quelle est l adresse de l hôpital Georges Pompidou"
 
     def test_remove_useless_words(self):
         assert self.PARSER.remove_useless_words(self.MESSAGE1) == "bonjour bot!! trouve tour eiffel?"
-        assert self.PARSER.remove_useless_words(self.MESSAGE2) == "salut grandpy tu connais adresse opera paris?"
-        assert self.PARSER.remove_useless_words(self.MESSAGE3) == "souhaiterais aller 59 boulevard strabourg, 75010 paris s'il plait."
+        assert self.PARSER.remove_useless_words(self.MESSAGE2) == "salut grandpy connais l'adresse l'opéra paris?"
+        assert self.PARSER.remove_useless_words(self.MESSAGE3) == "souhaiterais aller 59 boulevard strasbourg, 75010 paris s'il plaît."
         assert self.PARSER.remove_useless_words(self.MESSAGE4) == "veux allez visiter cathédrale notre-dame s'il plait."
+        assert self.PARSER.remove_useless_words(self.MESSAGE5) == "bonjour, l'adresse l'hôpital georges pompidou?"
 
-    # remove_useless_words + remove_punctuation integrated in function
+    def test_remove_accents(self):
+        assert self.PARSER.remove_accents(self.MESSAGE1) == "Bonjour bot!! Ou se trouve la Tour Eiffel?"
+        assert self.PARSER.remove_accents(self.MESSAGE2) == "Salut GrandPy Est-ce que tu connais l'adresse de l'Opera de Paris?"
+        assert self.PARSER.remove_accents(self.MESSAGE3) == "Je souhaiterais aller au 59 boulevard de Strasbourg, 75010 Paris s'il te plait."
+        assert self.PARSER.remove_accents(self.MESSAGE4) == "Je veux allez visiter la cathedrale de notre-dame s'il vous plait."
+        assert self.PARSER.remove_accents(self.MESSAGE5) == "Bonjour, quelle est l'adresse de l'hopital Georges Pompidou?"
+
+    # remove_useless_words + remove_punctuation + remove_accents integrated in function
     def test_remove_civilities(self):
-        assert self.PARSER.remove_civilities(self.MESSAGE1) == "ou se trouve la tour eiffel"
-        assert self.PARSER.remove_civilities(self.MESSAGE2) == "est-ce que tu connais l'adresse de l'opera de paris?"
-        assert self.PARSER.remove_civilities(self.MESSAGE3) == "je souhaiterais aller au 59 boulevard de strabourg, 75010 paris."
-        assert self.PARSER.remove_civilities(self.MESSAGE4) == "je veux allez visiter la cathédrale de notre-dame s'il vous plait."
+        assert self.PARSER.remove_civilities(self.MESSAGE1) == "trouve tour eiffel"
+        assert self.PARSER.remove_civilities(self.MESSAGE2) == "connais adresse opera paris"
+        assert self.PARSER.remove_civilities(self.MESSAGE3) == "souhaiterais aller 59 boulevard strasbourg 75010 paris"
+        assert self.PARSER.remove_civilities(self.MESSAGE4) == "veux allez visiter cathedrale dame"
+        assert self.PARSER.remove_civilities(self.MESSAGE5) == "adresse hopital georges pompidou"

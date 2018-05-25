fbb02b5c6c6e60311b842a80e9a3a64ce1ea6028
juliennuellas <julien.nuellas@gmail.com>
2018-05-25 18:36:45 +0200

    Directory structure for geocoding development feature OK

--- botapp/utils/geocoding.py
+++ botapp/utils/geocoding.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 # coding: utf-8
 from config import Config
+import requests
 
 class GeocodingLocation:
-    pass
+
+    def __init__(self, location):
+        self.location = location
+        api_key = Config.GEOCODING_GOOGLE_API_KEY
--- tests/utils/test_geocoding.py
+++ tests/utils/test_geocoding.py
@@ -3,4 +3,20 @@
 from botapp.utils.geocoding import GeocodingLocation
 
 class TestGeocodingLocation:
-    pass
+
+    def test_request_address_success(self, monkeypatch):
+        {
+           "results" : [
+              {
+                 "formatted_address" : "20 Rue Leblanc, 75015 Paris, France",
+                 "geometry" : {
+                    "location" : {
+                       "lat" : 48.8388508,
+                       "lng" : 2.2740328
+                    },
+                 },
+              }
+           ],
+           "status" : "OK"
+        }
+        pass

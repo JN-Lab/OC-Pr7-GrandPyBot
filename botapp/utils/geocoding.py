0675d4ca9d607fe1a259eb03eef6651ecb9dfe04
juliennuellas <julien.nuellas@gmail.com>
2018-05-28 13:52:14 +0200

    Geocoding request OK

--- botapp/utils/geocoding.py
+++ botapp/utils/geocoding.py
@@ -5,6 +5,33 @@ import requests
 
 class GeocodingLocation:
 
-    def __init__(self, location):
-        self.location = location
-        api_key = Config.GEOCODING_GOOGLE_API_KEY
+    def __init__(self):
+        self.api_key = Config.GEOCODING_GOOGLE_API_KEY
+
+    def get_location_info(self, location):
+        location = location.replace(" ", "+")
+
+        location_info = {
+            "address" : "",
+            "latitude" : 0,
+            "longitude" : 0,
+            "status" : ""
+        }
+
+        url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&components=country:FR&key=" + self.api_key
+
+        req = requests.get(url)
+        data = req.json()
+
+        try:
+            if data["results"][0]["geometry"]["location_type"] != "APPROXIMATE":
+                location_info["address"] = data["results"][0]["formatted_address"]
+                location_info["latitude"] = data["results"][0]["geometry"]["location"]["lat"]
+                location_info["longitude"] = data["results"][0]["geometry"]["location"]["lng"]
+                location_info["status"] = "found"
+            else:
+                raise KeyError
+        except KeyError:
+            location_info["status"] = "not found"
+
+        return location_info
--- tests/utils/test_geocoding.py
+++ tests/utils/test_geocoding.py
@@ -1,22 +1,34 @@
 #! /usr/bin/env python3
 # coding: utf-8
 from botapp.utils.geocoding import GeocodingLocation
+import requests
 
 class TestGeocodingLocation:
 
     def test_request_address_success(self, monkeypatch):
-        {
-           "results" : [
-              {
-                 "formatted_address" : "20 Rue Leblanc, 75015 Paris, France",
-                 "geometry" : {
-                    "location" : {
-                       "lat" : 48.8388508,
-                       "lng" : 2.2740328
-                    },
-                 },
-              }
-           ],
-           "status" : "OK"
+        results = {
+            "address" : "20 Rue Leblanc, 75015 Paris, France",
+            "latitude" : 48.8388508,
+            "longitude" : 2.2740328,
+            "status" : "found"
         }
-        pass
+
+        def mockreturn(request):
+            return results
+
+        req = GeocodingLocation()
+        assert req.get_location_info("hopital georges pompidou") == results
+
+    def test_request_address_false(self, monkeypatch):
+        results = {
+            "address" : "",
+            "latitude" : 0,
+            "longitude" : 0,
+            "status" : "not found"
+        }
+
+        def mockreturn(request):
+            return results
+
+        req = GeocodingLocation()
+        assert req.get_location_info("fake location") == results

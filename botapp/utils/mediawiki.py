0c6b1427f8b5b4b93575400ef10119eba1a47024
juliennuellas <julien.nuellas@gmail.com>
2018-05-28 17:44:18 +0200

    Geocoding features and mock test ok

---
+++ botapp/utils/mediawiki.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# coding: utf-8
+import requests
+
+class MediaWikiInfo:
+    pass
+
+"""
+https://en.wikipedia.org/w/api.php?action=query&prop=pageterms&colimit=50&wbptterms=description&generator=geosearch&ggscoord=48.8388508|2.2740328&gsradius=10000&gslimit=10
+"""
--- tests/utils/test_geocoding.py
+++ tests/utils/test_geocoding.py
@@ -2,10 +2,30 @@
 # coding: utf-8
 from botapp.utils.geocoding import GeocodingLocation
 import requests
+from io import BytesIO
+import json
 
 class TestGeocodingLocation:
 
     def test_request_address_success(self, monkeypatch):
+        # JSON response from the Google Geocoding API
+        req_result = {
+            "results" : [
+                {
+                    "formatted_address" : "20 Rue Leblanc, 75015 Paris, France",
+                    "geometry" : {
+                        "location" : {
+                            "lat" : 48.8388508,
+                            "lng" : 2.2740328
+                        },
+                    "location_type" : "ROOFTOP",
+                    },
+
+                }
+            ],
+        }
+
+        # Result we're looking for
         results = {
             "address" : "20 Rue Leblanc, 75015 Paris, France",
             "latitude" : 48.8388508,
@@ -14,12 +34,31 @@ class TestGeocodingLocation:
         }
 
         def mockreturn(request):
-            return results
+            return BytesIO(json.dumps(req_result).encode())
 
-        req = GeocodingLocation()
-        assert req.get_location_info("hopital georges pompidou") == results
+        script = GeocodingLocation()
+        monkeypatch.setattr(requests, 'Response', mockreturn)
+        assert script.get_location_info("hopital georges pompidou") == results
 
     def test_request_address_false(self, monkeypatch):
+        # JSON response from the Google Geocoding API
+        req_result = {
+            "results" : [
+                {
+                    "formatted_address" : "France",
+                    "geometry" : {
+                        "location" : {
+                            "lat" : 46.227638,
+                            "lng" : 2.213749
+                        },
+                    "location_type" : "APPROXIMATE",
+                    },
+
+                }
+            ],
+        }
+
+        # Result we're looking for
         results = {
             "address" : "",
             "latitude" : 0,
@@ -28,7 +67,8 @@ class TestGeocodingLocation:
         }
 
         def mockreturn(request):
-            return results
+            return json.JSONEncoder().encode(req_result)
 
         req = GeocodingLocation()
+        monkeypatch.setattr(requests, 'Response', mockreturn)
         assert req.get_location_info("fake location") == results

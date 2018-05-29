b791c18d35e67cdb73fb17395f4f49357645d0e3
juliennuellas <julien.nuellas@gmail.com>
2018-05-29 11:05:58 +0200

    TDD MediaWikiInfo : first test created

--- botapp/utils/mediawiki.py
+++ botapp/utils/mediawiki.py
@@ -3,8 +3,23 @@
 import requests
 
 class MediaWikiInfo:
-    pass
+
+    def get_story_info(self, latitude, longitude):
+        pass
 
 """
-https://en.wikipedia.org/w/api.php?action=query&prop=pageterms&colimit=50&wbptterms=description&generator=geosearch&ggscoord=48.8388508|2.2740328&gsradius=10000&gslimit=10
+https://fr.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&generator=geosearch&ggscoord=48.8388508|2.2740328&ggsradius=100&contentmodel=json
+"""
+
+"""
+https://fr.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&generator=geosearch&ggscoord=48.8388508|2.2740328&ggsradius=100
+"""
+
+"""
+https://fr.wikipedia.org/w/api.php?action=parse&text=%7B%7B%5Cu00c9bauche%7Ch%5Cu00f4pital%7CParis%7D%7D%5Cn%7B%7BInfobox%20H%5Cu00f4pital%5Cn%20%7C%20nom%20%20%20%20%20%20%20%20%20%20%20%20%3D%20H%5Cu00f4pital%20europ%5Cu00e9en%20Georges-Pompidou%5Cn%20%7C%20logo%20%20%20%20%20%20%20%20%20%20%20%3D%20%5Cn%20%7C%20logo%20taille%20%20%20%20%3D%20%5Cn%20%7C%20image%20%20%20%20%20%20%20%20%20%20%3D%20H%5Cu00f4pital%20europ%5Cu00e9en%20Georges-Pompidou%20logo.JPG%5Cn%20%7C%20l%5Cu00e9gende%20%20%20%20%20%20%20%20%3D%20Logo%20de%20l%27%5B%5BAssistance%20publique%20-%20h%5Cu00f4pitaux%20de%20Paris%5D%5D%20sur%20l%27H%5Cu00f4pital%20europ%5Cu00e9en%20Georges-Pompidou%5Cn%20%7C%20longitude%20%20%20%20%20%20%3D%202.2737%5Cn%20%7C%20latitude%20%20%20%20%20%20%20%3D%2048.8389%5Cn%20%7C%20pays%20%20%20%20%20%20%20%20%20%20%20%3D%20%7B%7BFrance%7D%7D%5Cn%20%7C%20ville%20%20%20%20%20%20%20%20%20%20%3D%20%5B%5BParis%5D%5D%5Cn%20%7C%20adresse%20%20%20%20%20%20%20%20%3D%2020%2C%20%5B%5Brue%20Leblanc%5D%5D%2075015%5Cn%20%7C%20date%20fondation%20%3D%20%5B%5B2001%5D%5D%5Cn%20%7C%20date%20fermetsure%20%3D%20%5Cn%20%7C%20site%20web%20%20%20%20%20%20%20%3D%20http%3A%2F%2Fhopital-georgespompidou.aphp.fr%2F%5Cn%20%7C%20type%20%20%20%20%20%20%20%20%20%20%20%3D%20%5Cn%20%7C%20assurance%20mal%20%20%3D%20%5Cn%20%7C%20affiliation%20%20%20%20%3D%20%5B%5BAssistance%20publique%20-%20h%5Cu00f4pitaux%20de%20Paris%7CAP-HP%5D%5D%20%5Cn%20%7C%20sta&contentmodel=wikitext&contentformat=text/plain
+"""
+
+"""
+Good One
+https://fr.wikipedia.org/w/api.php?action=query&prop=extracts|url&exintro&explaintext&generator=geosearch&ggscoord=48.8388508|2.2740328&ggsradius=100
 """
--- tests/utils/test_mediawiki.py
+++ tests/utils/test_mediawiki.py
@@ -1,5 +1,56 @@
 #! /usr/bin/env python3
 # coding: utf-8
-
 from botapp.utils.mediawiki import MediaWikiInfo
- 
+import requests
+from io import BytesIO
+import json
+
+class TestMediaWikiInfo:
+
+    def test_request_info_success(self, monkeypatch):
+        # JSON response from the Media Wiki API
+        req_result = {
+            "batchcomplete": "",
+            "warnings": {
+                "query": {
+                    "*": "Unrecognized value for parameter \"prop\": url."
+                }
+            },
+            "query": {
+                "pages": {
+                    "854335": {
+                        "pageid": 854335,
+                        "ns": 0,
+                        "title": "H\u00f4pital europ\u00e9en Georges-Pompidou",
+                        "index": 0,
+                        "extract": "L'h\u00f4pital europ\u00e9en Georges-Pompidou (HEGP) est un h\u00f4pital de l'Assistance publique - h\u00f4pitaux de Paris (AP-HP). Il est situ\u00e9 entre les rues Leblanc et du Professeur-Florian-Delbarre dans le 15e arrondissement de Paris, au bord de la Seine non loin du pont du Garigliano, \u00e0 proximit\u00e9 du parc Andr\u00e9-Citro\u00ebn et des locaux de France T\u00e9l\u00e9visions."
+                    },
+                    "2893691": {
+                        "pageid": 2893691,
+                        "ns": 0,
+                        "title": "Rue Leblanc",
+                        "index": 1,
+                        "extract": "La rue Leblanc est une rue du 15e arrondissement de Paris."
+                    }
+                }
+            }
+        }
+
+        # Result we're looking for
+        results = {
+            "title" : "Hôpital européen Georges-Pompidou",
+            "intro" : """L'hôpital européen Georges-Pompidou (HEGP) est un hôpital
+                        de l'Assistance publique - hôpitaux de Paris (AP-HP). Il est
+                        situé entre les rues Leblanc et du Professeur-Florian-Delbarre
+                        dans le 15e arrondissement de Paris, au bord de la Seine non loin du pont
+                        du Garigliano, à proximité du parc André-Citroën et des
+                        locaux de France Télévisions.""",
+            "page_link" : "https://fr.wikipedia.org/wiki/H%C3%B4pital_europ%C3%A9en_Georges-Pompidou"
+        }
+
+        def mockreturn(request):
+            return BytesIO(json.dumps(req_result).encode())
+
+        script = MediaWikiInfo()
+        monkeypatch.setattr(requests, 'Response', mockreturn)
+        assert script.get_story_info(48.8388508, 2.2740328) == results

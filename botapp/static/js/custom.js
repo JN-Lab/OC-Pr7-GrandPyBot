f69d8d3a96d55532c91f99529f3bd3daddca6faf
juliennuellas <julien.nuellas@gmail.com>
2018-06-08 17:56:42 +0200

    All CSS migration on sass DONE

--- botapp/static/js/custom.js
+++ botapp/static/js/custom.js
@@ -9,20 +9,16 @@ var chatContainerElt = document.getElementById("chat-container");
 
 // Click on Chat Button
 chatButtonElt.addEventListener("click", function() {
-  chatButtonElt.style.color = "#F9AA33";
-  chatButtonElt.style.backgroundColor = "#FFFFFF";
-  infoButtonElt.style.color = "#232F34";
-  infoButtonElt.style.backgroundColor = "#CFD8DC";
+  chatButtonElt.className = "message-nav-element nav-selected";
+  infoButtonElt.className = "message-nav-element nav-unselected";
   chatContainerElt.style.display = "flex";
   infoContainerElt.style.display = "none";
 });
 
 // Click on Info Button
 infoButtonElt.addEventListener("click", function() {
-  infoButtonElt.style.color = "#F9AA33";
-  infoButtonElt.style.backgroundColor = "#FFFFFF";
-  chatButtonElt.style.color = "#232F34";
-  chatButtonElt.style.backgroundColor = "#CFD8DC";
+  chatButtonElt.className = "message-nav-element nav-unselected";
+  infoButtonElt.className = "message-nav-element nav-selected";
   infoContainerElt.style.display = "flex";
   chatContainerElt.style.display = "none";
 });
